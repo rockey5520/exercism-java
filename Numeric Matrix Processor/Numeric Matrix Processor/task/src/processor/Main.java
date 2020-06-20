6de25fb30473e08c1a60a7387aabffbe2c8aedd8
Rakesh Mothukuri <rockey5520@gmail.com>
2020-06-20 11:33:09 +0100

    stage 2

--- Numeric Matrix Processor/Numeric Matrix Processor/task/src/processor/Main.java
+++ Numeric Matrix Processor/Numeric Matrix Processor/task/src/processor/Main.java
@@ -6,8 +6,30 @@ public class Main {
 
   public static void main(String[] args) {
     //stage 2
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int m = sc.nextInt();
+    int[][] matrixA = new int[n][m];
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        matrixA[i][j] = sc.nextInt();
+      }
+    }
+    int c = sc.nextInt();
 
-    //Stage 1
+    for (int[] integer : matrixA
+    ) {
+      for (Integer integer1 : integer
+      ) {
+        System.out.print(integer1 * c);
+        System.out.print(" ");
+      }
+      System.out.println();
+    }
+  }
+
+}
+//Stage 1
     /*Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
     int m = sc.nextInt();
@@ -35,5 +57,3 @@ public class Main {
       }
       System.out.println();
     }*/
-  }
-}
