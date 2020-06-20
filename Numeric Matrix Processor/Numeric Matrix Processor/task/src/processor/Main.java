e1ea50f6ff5c89b4be02b3d4160d17f90384a966
Rakesh Mothukuri <rockey5520@gmail.com>
2020-06-20 11:28:43 +0100

    initial commit

---
+++ Numeric Matrix Processor/Numeric Matrix Processor/task/src/processor/Main.java
@@ -0,0 +1,39 @@
+package processor;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    //stage 2
+
+    //Stage 1
+    /*Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int m = sc.nextInt();
+    int[][] matrixA = new int[n][m];
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        matrixA[i][j] = sc.nextInt();
+      }
+    }
+    if (n != sc.nextInt() || m != sc.nextInt()) {
+      System.out.println("ERROR");
+      return;
+    }
+    int[][] matrixB = new int[n][m];
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        matrixB[i][j] = sc.nextInt();
+      }
+    }
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        System.out.print(matrixA[i][j] + matrixB[i][j]);
+        System.out.print(" ");
+      }
+      System.out.println();
+    }*/
+  }
+}
